2d93f1b620a8e68b6f5dd64c713b2604eaf4891b
Tony <95930252+GuanTony@users.noreply.github.com>
2023-02-23 23:19:02 +0100

    Add files via upload

--- Questionnaire.js
+++ Questionnaire.js
@@ -1,6 +1,4 @@
-
 function openQuest() {
-
     document.getElementById("choixmatin").style.display = "block";
 }
 
@@ -8,18 +6,9 @@ function closeQuest() {
     document.getElementById("choixmatin").style.display = "none";
 }
 
-suivantmatin = document.getElementById("suivantmatin");
-suivantmatin.onclick = function () {
-    {
-        document.getElementById("matin").style.display = "none";
-        document.getElementById("choixmatin").style.display = "none";
-        document.getElementById("diner").style.display = "block";
-        document.getElementById("suivantmatin").style.display = "none";
-        const btnList = document.getElementsByClassName("btn");
-        for (const btn of btnList) {
-            btn.style.display = "block";
+$(document).ready(function () {
 
-        }
-
-    }
-}
+    $(".non").click(function () {
+        $(".clear").prop("checked", false);
+    });
+});
