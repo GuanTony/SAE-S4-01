02c5bec5bbff6ecc463b87f2145d66bf7d9d9644
Tony <95930252+GuanTony@users.noreply.github.com>
2023-02-25 22:00:21 +0100

    Add files via upload

--- Questionnaire.js
+++ Questionnaire.js
@@ -4,7 +4,13 @@ function openQuest() {
 
 function closeQuest() {
     document.getElementById("choixmatin").style.display = "none";
+    document.getElementById("Produit Laitier").style.display = "none";
+    document.getElementById("Fruit").style.display = "none";
+    document.getElementById("Boisson1").style.display = "none";
+    document.getElementById("Patisserie").style.display = "none";
 }
+
+
 
 $(document).ready(function () {
 
@@ -12,3 +18,111 @@ $(document).ready(function () {
         $(".clear").prop("checked", false);
     });
 });
+
+function showPL(){
+    if (document.getElementById("pl").checked==true)
+    {
+        document.getElementById("Produit Laitier").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Produit Laitier").style.display = "none";
+    }
+}
+function showfruit() {
+
+    if (document.getElementById("fru").checked== true) {
+        document.getElementById("Fruit").style.display = "block";
+    } else {
+        document.getElementById("Fruit").style.display = "none";
+    }
+}
+
+function showboisson1(){
+    if(document.getElementById("bs1").checked==true){
+        document.getElementById("Boisson1").style.display = "block";
+}
+    else{
+        document.getElementById("Boisson1").style.display = "none"
+    }
+}
+
+function showpatisserie() {
+    if (document.getElementById("pts").checked == true) {
+        document.getElementById("Patisserie").style.display = "block";
+    } else {
+        document.getElementById("Patisserie").style.display = "none"
+    }
+}
+function showentree(){
+    if (document.getElementById("ent").checked==true)
+    {
+        document.getElementById("Entree").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Entree").style.display = "none";
+    }
+}
+function showviande(){
+    if (document.getElementById("via").checked==true)
+    {
+        document.getElementById("Viande").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Viande").style.display = "none";
+    }
+}
+function showpoisson(){
+    if (document.getElementById("poi").checked==true)
+    {
+        document.getElementById("Poisson").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Poisson").style.display = "none";
+    }
+}
+
+function showpates(){
+    if (document.getElementById("pat").checked==true)
+    {
+        document.getElementById("Pates").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Pates").style.display = "none";
+    }
+}
+
+function showcompose(){
+    if (document.getElementById("com").checked==true)
+    {
+        document.getElementById("Compose").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Compose").style.display = "none";
+    }
+}
+function showfromage(){
+    if (document.getElementById("fro").checked==true)
+    {
+        document.getElementById("Fromage").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Fromage").style.display = "none";
+    }
+}
+function showdessert(){
+    if (document.getElementById("des").checked==true)
+    {
+        document.getElementById("Dessert").style.display = "block";
+    }
+    else
+    {
+        document.getElementById("Dessert").style.display = "none";
+    }
+}
