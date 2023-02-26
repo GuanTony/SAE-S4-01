33e37848cd5126066129159caba813c231016646
JoseMendesP <115621656+JoseMendesP@users.noreply.github.com>
2023-02-26 21:02:35 +0100

    Update Aliment.php

--- Aliment.php
+++ Aliment.php
@@ -1,17 +1,24 @@
 <?php
-  session_start(); 
+  session_start();
+  $Produit =  isset($_POST['Produit'])?($_POST['Produit']):'';
+  $Fruit =  isset($_POST['Fruit'])?($_POST['Fruit']):'';
+  $Boisson1 =  isset($_POST['Boisson1'])?($_POST['Boisson1']):'';
+  $Patisserie=  isset($_POST['Patisserie'])?($_POST['Patisserie']):'';
   $Entree =  isset($_POST['Entree'])?($_POST['Entree']):'';
   $Viande =  isset($_POST['Viande'])?($_POST['Viande']):'';
   $Poisson =  isset($_POST['Poisson'])?($_POST['Poisson']):'';
   $Pates=  isset($_POST['Pates'])?($_POST['Pates']):'';
   $Compose=  isset($_POST['Compose'])?($_POST['Compose']):'';
+  $Fromage=  isset($_POST['Fromage'])?($_POST['Fromage']):'';
+  $Dessert=  isset($_POST['Dessert'])?($_POST['Dessert']):'';
+  $Boisson2=  isset($_POST['Boisson2'])?($_POST['Boisson2']):'';
   $msg = '';
   
 
   if  (count($_POST)==0)
       require ('Aliment.html');
   else{
-        if(!ajout($Entree,$Viande,$Poisson,$Pates,$Compose)){
+        if(!ajout($Produit,$Fruit,$Boisson1,$Patisserie,$Entree,$Viande,$Poisson,$Pates,$Compose,$Fromage,$Dessert,$Boisson2)){
             $msg = "ajout impossible veuillez réesayer plus tard";
             require ('Aliment.html');
         } else{
@@ -20,17 +27,17 @@
         }
   }
 
-    function ajout($Entree,$Viande,$Poisson,$Pates,$Compose){
+    function ajout($Produit,$Fruit,$Boisson1,$Patisserie,$Entree,$Viande,$Poisson,$Pates,$Compose,$Fromage,$Dessert,$Boisson2){
       require('connectSQL.php');
-      $sql = "INSERT INTO `Aliment` (Entree,Viande, Poisson, Pates, Compose) VALUES (?,?,?,?,?)";
+      $sql = "INSERT INTO `aliments` (`Produit Laitier`,`Fruit`,`Boisson1`,`Patisserie`,`Entree`,`Viande`,`Poisson`,`Pates`,`Compose`,`Fromage`,`Dessert`,`Boisson2`,`ID`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
           try{
               $commande = $pdo->prepare($sql);
-              $exec = $commande->execute([$Entree,$Viande,$Poisson,$Pates,$Compose]);
+              $exec = $commande->execute([$Produit,$Fruit,$Boisson1,$Patisserie,$Entree,$Viande,$Poisson,$Pates,$Compose,$Fromage,$Dessert,$Boisson2,$_SESSION[`ID`]]);
               getID();
               return true;
           }
           catch (PDOException $e) {
-              echo utf8_encode("Echec de select : " . $e->getMessage() . "\n");
+              echo utf8_encode("Echec de Insert : " . $e->getMessage() . "\n");
               die(); // On arrête tout.
           }
           return false;
@@ -51,6 +58,10 @@
           $_SESSION[`ID`]=$resultat[0]["MAX(ID)"];
       }
 ?>
+
+
+
+
 
 
 
